98e0f2cd7b27d5cba3b1efa09e04f74783b87c3c
Bimal1993 <arbimalkumar.1993@gmail.com>
2022-05-03 10:57:14 +0530

    adding for docker test

--- src/main/java/com/training/Consumer.java
+++ src/main/java/com/training/Consumer.java
@@ -3,7 +3,10 @@ package com.training;
 public class Consumer {
 
 	public void reveive() {
-		System.out.println("Message Recived");
+		System.out.println("Message Recived 1");
+		System.out.println("Message Recived 2");
+		System.out.println("Message Recived 3");
+		System.out.println("Message Recived 4");
 	}
 
 	public boolean isRev() {
